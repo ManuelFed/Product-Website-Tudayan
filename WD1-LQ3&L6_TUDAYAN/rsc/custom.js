75609a93ab685f7ce6ab5d636c2a8598a685d010
Manuel Federico Tudayan <147285801+ManuelFed@users.noreply.github.com>
2025-11-15 04:24:49 +0800

    Delete WD1-LQ3&L6_TUDAYAN directory

--- WD1-LQ3&L6_TUDAYAN/rsc/custom.js
+++
@@ -1,7 +0,0 @@
-// Attach click handler to Buy Now button
-document.getElementsByClassName('add').addEventListener('click', addButton);
-
-// Show confirmation message when Buy Now is clicked
-function addButton() {
-    alert("Item has been added to cart")
-}
